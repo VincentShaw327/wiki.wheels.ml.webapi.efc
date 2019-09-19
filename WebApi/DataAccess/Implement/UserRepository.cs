f8e8f69fce907ccb5550eafd585a099f6f598db8
VincentShaw327 <vincentshaw0327@gmail.com>
2019-09-19 20:49:16 +0800

    添加操作用户的接口映射

---
+++ WebApi/DataAccess/Implement/UserRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+using WebApi.Entities;
+using WebApi.DataAccess.Interface;
+using WebApi.DataAccess.Base;
+
+namespace WebApi.DataAccess.Implement
+{
+    public class UserRepository : IUserRepository
+    {
+        //private readonly VueContext _context;
+        public readonly AlanContext _context;
+
+        public UserRepository(AlanContext context)
+        {
+            _context = context;
+        }
+
+        public User GetUserByEmail(string email)
+        {
+            return _context.User.SingleOrDefault(x => x.Email == email);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.User;
+        }
+
+        public User GetById(string id)
+        {
+            return _context.User.Find(id);
+        }
+
+        public bool Any(Expression<Func<User, bool>> predicate)
+        {
+            return _context.User.Any(predicate);
+        }
+
+        public int Add(User user)
+        {
+            _context.User.Add(user);
+            return _context.SaveChanges();
+        }
+
+        public int Update(User user)
+        {
+            _context.User.Update(user);
+            return _context.SaveChanges();
+        }
+
+        public int Remove(User user)
+        {
+            _context.User.Remove(user);
+            return _context.SaveChanges();
+        }
+    }
+}
---
+++ WebApi/DataAccess/Interface/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+using WebApi.Entities;
+
+
+namespace WebApi.DataAccess.Interface
+{
+    interface IUserRepository
+    {
+        User GetUserByEmail(string email);
+        IEnumerable<User> GetAll();
+        User GetById(string id);
+        bool Any(Expression<Func<User, bool>> predicate);
+        int Add(User user);
+        int Update(User user);
+        int Remove(User user);
+    }
+}
