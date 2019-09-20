d888f033ab8b68679b85dc84eb24eec6de33223c
VincentShaw327 <vincentshaw0327@gmail.com>
2019-09-20 10:24:39 +0800

    添加JWT未调试正常前...

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 
@@ -12,11 +13,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 using WebApi.DataAccess.Base;
 using WebApi.DataAccess.Implement;
 using WebApi.DataAccess.Interface;
+using WebApi.Helpers;
 
 namespace WebApi
 {
@@ -34,8 +38,54 @@ namespace WebApi
         {
             services.AddAutoMapper();
 
+            // DI
+            services.AddScoped<IAlanDao, AlanDao>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            //注册数据库服务
             services.AddDbContext<AlanContext>(options => options.UseMySQL(Configuration.GetConnectionString("AlanConnection")));
             services.AddScoped<IAlanDao, AlanDao>();
+
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+
+            // configure jwt authentication
+            //var appSettings = appSettingsSection.Get<AppSettings>();
+            //var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            //services.AddAuthentication(x =>
+            //{
+            //    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //    .AddJwtBearer(x =>
+            //    {
+            //        x.Events = new JwtBearerEvents
+            //        {
+            //            OnTokenValidated = context =>
+            //            {
+            //                var userService = context.HttpContext.RequestServices.GetRequiredService<IAlanDao>();
+            //                var userId = context.Principal.Identity.Name;
+            //                var user = userService.GetById(userId);
+            //                if (user == null)
+            //                {
+            //                    // return unauthorized if user no longer exists
+            //                    context.Fail("Unauthorized");
+            //                }
+            //                return Task.CompletedTask;
+            //            }
+            //        };
+            //        x.RequireHttpsMetadata = false;
+            //        x.SaveToken = true;
+            //        x.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateIssuerSigningKey = true,
+            //            IssuerSigningKey = new SymmetricSecurityKey(key),
+            //            ValidateIssuer = false,
+            //            ValidateAudience = false
+            //        };
+            //    });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
