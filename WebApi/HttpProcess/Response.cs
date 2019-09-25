95adca6df308465eca1a3e2d894980609f68ce16
VincentShaw327 <vincentshaw0327@gmail.com>
2019-09-25 10:07:29 +0800

    添加数据返回格式类

---
+++ WebApi/HttpProcess/Response.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.HttpProcess
+{
+    public class Response
+    {
+        public int success;
+        public string message;
+        public object token;
+        public object dataList;
+
+        public Response()
+        {
+            success = 0;
+            message = "";
+            token = "";
+        }
+    }
+}
