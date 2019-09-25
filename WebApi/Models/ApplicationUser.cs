2b4271563ee57e48b04aeaa77c6aa1577160aee5
VincentShaw327 <vincentshaw0327@gmail.com>
2019-09-25 10:01:20 +0800

    添加用户与角色模型

---
+++ WebApi/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebApi.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //new public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //new public string UserName { get; set; }
+        //new public string Email { get; set; }
+        //new public byte[] PasswordHash { get; set; }
+        //public byte[] PasswordSalt { get; set; }
+    }
+}
