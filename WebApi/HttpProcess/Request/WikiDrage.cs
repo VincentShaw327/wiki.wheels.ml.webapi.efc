b351461593b913cff632a4658b48f9dbe6f21644
VincentShaw327 <vincentshaw0327@gmail.com>
2019-10-09 09:57:16 +0800

    新增拖拽接受参数对象

---
+++ WebApi/HttpProcess/Request/WikiDrage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.HttpProcess.Request
+{
+    public class WikiDrage
+    {
+        public bool dropToGap;
+        public int  dropPosition;
+        public int pid;
+        public int dragPid;
+        public int prePos;
+        public int id;
+        public WikiDrage()
+        {
+            dropToGap = false;
+            dropPosition = 0;
+            pid = 0;
+            dragPid = 0;
+            prePos = 0;
+            id = 0;
+        }
+    }
+}
